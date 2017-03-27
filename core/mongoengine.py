3b1643c35c74c6c8e330f0424cb6828e6661e881
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-03-27 09:30:03 +0100

    attempting to fix mongoengine error

--- core/mongoengine.py
+++ core/mongoengine.py
@@ -7,7 +7,7 @@ class Document(MongoDocument):
     meta = {'allow_inheritance': True}
 
     def clean(self):
-        for field in self._changed_fields:
+        for field in self._get_changed_fields():
             method = 'clean_{}'.format(field)
             if hasattr(self, method):
                 getattr(self, method)()
