4e8a1ca5a1eb48fc7a225dd400b3863fccd705ab
fuentesloic <fuentes.loic@gmail.com>
2017-04-07 10:00:10 -0400

    init map
    init css to around map with class nammed you to see directly where the user is
    
    init JS to around map with user special marker, class nammed you
    
    clean html to be readable correctly
    
    add a Json database to develop the js around code
    
    install webworker to respond interesting point positions from test.json
    
    new webworker to run and receive position then create a classic marker
    
    add popup with name of the point
    
    complete popup with set html and prepare place for put data from webworker
    
    add wifi and powerAvability
    
    received wifi and powerAvaibility data
    
    fix the probleme with comment because it's an array
    
    add content into html popup
    
    remove current position sentence - useless
    
    set zoom and center view on the current location
    
    add a css link to trip
    
    overise trips.css for red marker and current position style with z-index
    
    change mostly syntax error from vinyll review

---
+++ static/js/webworker-around.js
@@ -0,0 +1,16 @@
+onmessage = _ => {
+  fetch('/static/test.json')
+    .then(response => response.json())
+    .then(json => {
+      const items = json
+      items.forEach(item => {
+        let name = item.name
+        let wifi = item.wifiQuality
+        let power = item.powerAvailable
+        let comment = item.comments[0]
+        let lng = item.position.longitude
+        let lat = item.position.latitude
+        postMessage([name,wifi,power,comment,lng,lat])
+      })
+    })
+}
