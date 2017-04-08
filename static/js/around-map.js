4709f06a00f88680e2073990f30872599cc1042f
fuentesloic <fuentes.loic@gmail.com>
2017-04-08 09:42:50 -0400

    new webworker to run and receive position then create a classic marker
    add popup with name of the point
    
    complete popup with set html and prepare place for put data from webworker
    
    add wifi and powerAvability
    
    received wifi and powerAvaibility data
    
    fix the probleme with comment because it's an array
    
    add content into html popup
    
    remove current position sentence - useless
    
    set zoom and center view on the current location
    
    add a css link to trip
    
    overise trips.css for red marker and current position style with z-index
    
    change mostly syntax error from vinyll review

--- static/js/around-map.js
+++ static/js/around-map.js
@@ -6,17 +6,47 @@ const map = new mapboxgl.Map({
   zoom: 2
 })
 
-// user last location
-if(current_location.length) {
+// users interesting points
+const pois = new Worker('/static/js/webworker-around.js');
+
+pois.onmessage = informations => {
+  const [name, wifi, power, comment, lng, lat] = informations.data
+
+  const el = document.createElement('div')
+  el.classList.add('marker')
+
+  let content = `<h2>Hello ${name}</h2>
+                 <ul>
+                    <li>Wifi quality: ${wifi}</li>
+                    <li>Power available: ${power}</li>
+                    <li>Comments: ${comment}</li>
+                  </ul>`
+
   const popup = new mapboxgl.Popup({offset: [10, -20]})
-      .setText('you')
+      .setHTML(content)
+
+  new mapboxgl.Marker(el, {offset:[0, -30]})
+      .setLngLat([lng, lat])
+      .setPopup(popup)
+      .addTo(map)
+};
+
+pois.postMessage('info-requested');
+
+// user last location
+if(currentLocation.length) {
+  const popup = new mapboxgl.Popup({offset: [10, -20]})
+      .setText('You current location')
 
   const el = document.createElement('div');
   el.classList.add('marker')
-  el.classList.add('you')
+  el.classList.add('current-location')
 
   new mapboxgl.Marker(el, {offset:[0, -30]})
-      .setLngLat(current_location.reverse())
+      .setLngLat(currentLocation.reverse())
       .setPopup(popup)
       .addTo(map)
+
+  map.setCenter(currentLocation)
+  map.setZoom(11)
 }
