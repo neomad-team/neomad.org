71797ca63b98d717dbbdf87009f55aaf1c6bd6e4
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-04-12 19:10:41 +0200

    proxied webnomad spots in json

--- around/views.py
+++ around/views.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from flask import render_template
 
 from core import app
@@ -8,3 +11,14 @@ from user.models import User
 def around():
     users = User.objects(allow_localization=True, current_location__size=2)
     return render_template('around/map.html', users=users)
+
+
+@app.route('/around/spots.json')
+def data_spots():
+    data = requests.get('http://158.58.170.155:3030/spots')
+    response = app.response_class(
+        response=data.content,
+        status=data.status_code,
+        mimetype='application/json'
+    )
+    return response
