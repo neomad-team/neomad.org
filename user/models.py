14f43a6da53616a0321a24c09cae4b9512e3977c
Hugo MOCHET <mochet.hugo@gmail.com>
2017-04-17 14:27:25 +0200

    The username is now set with the email

--- user/models.py
+++ user/models.py
@@ -43,5 +43,7 @@ class User(UserMixin, db.Document):
         return str(self.username or 'Unknown')
 
     def save(self, *args, **kwargs):
+        if self.username is None:
+            self.username = self.email.split('@')[0]
         self.slug = slugify(self.username)
         return super().save(*args, **kwargs)
