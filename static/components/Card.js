2697de77d58bb7a4e35d051db7af498737047082
fuentesloic <fuentes.loic@gmail.com>
2017-04-27 16:02:24 -0400

    place currentPosition in the state

--- static/components/Card.js
+++ static/components/Card.js
@@ -2,17 +2,12 @@ import React from 'react'
 
 class Card extends React.Component {
 
-  closeCard = _ => {
-
-  }
-
   render() {
     return (
       <div className="card">
         <h2>{this.props.details.name}</h2>
         <ul>
-          <li>power avaibality: {this.props.details.powerAvailable}</li>
-          <li>Wifi quality: {this.props.details.wifiQuality}/5</li>
+          <li></li>
         </ul>
       </div>
     )
