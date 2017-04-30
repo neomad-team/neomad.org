d33e6c647e0be1750cf6dc3ef9515fdff0d2aaa6
Hugo MOCHET <mochet.hugo@gmail.com>
2017-04-30 14:42:05 +0200

    Remove folder's pictures when deleting an article
    Add test for this feature

--- blog/test.py
+++ blog/test.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from .models import Article
@@ -20,3 +21,9 @@ class ArticleTest(TestCase):
         article = Article(title='',
                           content='<p><em>emphased content</em><i>also emphased</i></p>').save()
         self.assertEqual(article.content, '<p><em>emphased content</em><em>also emphased</em></p>')
+
+    def test_delete_article_and_folders_pictures(self):
+        article = Article(title='<h1>title<br></h1>', content='<p>content</p>').save()
+        self.assertTrue(os.path.isdir(article.get_images_path()))
+        Article.objects.get(id=article.id).delete()
+        self.assertFalse(os.path.isdir(article.get_images_path()))
