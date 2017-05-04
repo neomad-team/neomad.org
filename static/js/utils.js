3d8e8423bb181fa20ef42901cd19303b4041cec1
fuentesloic <fuentes.loic@gmail.com>
2017-05-04 11:57:51 -0400

    display menu only with click event

--- static/js/utils.js
+++ static/js/utils.js
@@ -18,21 +18,7 @@ function alert(type, message, delay) {
 window.onload = _ => {
   const menu = document.getElementById('menu')
   const avatar = document.getElementById('avatarMenu')
-  const main = document.getElementsByTagName('main')
-  const active = _ => {
-    menu.classList.toggle('active')
-  }
   avatar.addEventListener('click', function() {
-    active()
-  })
-  avatar.addEventListener('mouseenter', function() {
-    if (menu.className == 'profile-menu') {
-      active()
-    }
-  })
-  menu.addEventListener('mouseleave', function() {
-    if (menu.className == 'profile-menu active') {
-      active()
-    }
+    menu.classList.toggle('active')
   })
 }
