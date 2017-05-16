90726d1390b6f80ad6120b1d2c628423a2a5a4c9
Hugo MOCHET <mochet.hugo@gmail.com>
2017-05-16 17:06:33 +0200

    Add test for authentication

--- auth/test.py
+++ auth/test.py
@@ -8,12 +8,24 @@ from user.views import *
 class AuthTest(TestCase):
     def setUp(self):
         self.client = app.test_client()
+        self.user = User(email='test@email.com').set_password('test').save()
 
     def tearDown(self):
         User.objects.delete()
 
-    def test_signup_same_username_email(self):
-        # Sign up a new user
+    def test_signup(self):
+        data = {
+            'email': 'newtest@email.com',
+            'password': 'test'
+        }
+        result = self.client.post('/signup', data=data)
+        try:
+            User.objects.get(email='newtest@email.com')
+        except User.DoesNotExist:
+            self.fail('User does not exist')
+        self.assertEqual(result.status_code, 302)
+
+    def test_signup_with_same_slug(self):
         data = {
             'email': 'newtest@email.com',
             'password': 'test'
@@ -52,3 +64,27 @@ class AuthTest(TestCase):
         except User.DoesNotExist:
             self.fail('User does not exist')
         self.assertEqual(result.status_code, 302)
+
+    def test_login_get(self):
+        result = self.client.get('/login')
+        self.assertEqual(result.status_code, 200)
+
+    def test_login_post(self):
+        data = {
+            'email': 'test@email.com',
+            'password': 'test',
+        }
+        result = self.client.post('/login', data=data)
+        self.assertEqual(result.status_code, 302)
+
+    def test_wrong_login_post(self):
+        data = {
+            'email': 'wrong@email.com',
+            'password': 'test',
+        }
+        result = self.client.post('/login', data=data)
+        self.assertEqual(result.status_code, 401)
+
+    def test_logout(self):
+        result = self.client.get('/logout')
+        self.assertEqual(result.status_code, 200)
