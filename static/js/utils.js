ecdbb9dc147c32391637e8aaa971d21df8305225
fuentesloic <fuentes.loic@gmail.com>
2017-06-27 08:43:12 -0400

    click on avatar bug - js stop cause /around

--- static/js/utils.js
+++ static/js/utils.js
@@ -17,17 +17,19 @@ function alert(type, message, delay) {
 
 window.onload = _ => {
   // section poisCards hidden marker overflow
-  const canvas = document.querySelector('canvas')
-  const poisCards = document.querySelector('#poi-cards')
-  poisCards.style.minHeight = `${canvas.height}px`
+  if(window.location.href.includes('around')) {
+    const canvas = document.querySelector('canvas')
+    const poisCards = document.querySelector('#poi-cards')
+    poisCards.style.minHeight = `${canvas.height}px`
 
-  const displayForm = document.querySelectorAll('.display-form')
-  displayForm.forEach( button => {
-    button.addEventListener('click', _ => {
-      const poiForm = document.querySelector('#poi-form')
-      poiForm.classList.toggle('visible')  
+    const displayForm = document.querySelectorAll('.display-form')
+    displayForm.forEach( button => {
+      button.addEventListener('click', _ => {
+        const poiForm = document.querySelector('#poi-form')
+        poiForm.classList.toggle('visible')  
+      })
     })
-  })
+  }
   
   const menu = document.querySelector('#menu')
   const avatar = document.querySelector('#avatar-menu')
