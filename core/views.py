c64344d241f8ee3876494d8c38e5b44bf2bd669b
fuentesloic <fuentes.loic@gmail.com>
2017-07-04 18:54:18 -0400

    new button to go to register design place and responsive

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from flask import redirect, render_template
 from flask_login import current_user
 
+from blog.models import Article
 from user.models import User
 from . import app
 from .helpers import url_for_user
 
 
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    articles = Article.objects.all()[:3]
+    return render_template('home.html', articles=articles)
