8d596215a5959a9e32e443bb09916a91b9898486
Hugo MOCHET <mochet.hugo@gmail.com>
2017-07-05 10:38:31 +0200

    Fix json response for POST /api/spot

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import request, Response
 from flask_login import current_user
 
@@ -5,15 +7,31 @@ from core import app
 from around.models import Spot
 
 
+def create_json_response(spot):
+    json_response = {
+        'id': str(spot.id),
+        'name': spot.name,
+        'wifi': spot.wifi_quality,
+        'user': str(spot.user.id),
+        'location': {'latitude': spot.location[0],
+                     'longitude': spot.location[1]},
+        'power': spot.power_available,
+        'type': spot.category,
+        'comment': spot.comments,
+        'date': spot.creation_date.strftime("%Y-%m-%d")
+    }
+    return json_response
+
+
 @app.route('/api/spots')
 def spots():
-    spots = Spot.objects.all().to_json()
+    spots = [create_json_response(spot) for spot in Spot.objects.all()]
     return Response(spots, mimetype='application/json')
 
 
 @app.route('/api/spot', methods=['post'])
 def spot_create():
-    response = request.json
+    response = request.get_json()
     spot = Spot(
         name=response.get('name'),
         wifi=response.get('wifi'),
@@ -23,4 +41,4 @@ def spot_create():
         category=response.get('type'),
         comments=[response.get('comment')]
     ).save()
-    return spot.to_json(), 201
+    return json.dumps(create_json_response(spot)), 201
