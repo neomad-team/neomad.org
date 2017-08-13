5fce1aac5466ce730fd572638ff32e8bd9c414bd
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-08-13 19:36:59 +0200

    added location name for easy privacy locations editing

--- static/js/utils.js
+++ static/js/utils.js
@@ -15,14 +15,31 @@ function alert(type, message, delay) {
   }
 }
 
+function coordinatesToAddress (coordinates) {
+  const [lat, lng] = coordinates.split(',')
+  return fetch(`http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`, {
+    mode: 'cors'
+  })
+  .then(r => r.json())
+  .then(d => d.address)
+}
+
 window.onload = _ => {
   // section poisCards hidden marker overflow
   if(window.location.href.includes('around')) {
     const canvas = document.querySelector('canvas')
     const poisCards = document.querySelector('#poi-cards')
     poisCards.style.minHeight = `${canvas.height}px`
+
+    const displayForm = document.querySelectorAll('.display-form')
+    displayForm.forEach( button => {
+      button.addEventListener('click', _ => {
+        const poiForm = document.querySelector('#poi-form')
+        poiForm.classList.toggle('visible')
+      })
+    })
   }
-  
+
   const menu = document.querySelector('#menu')
   const avatar = document.querySelector('#avatar-menu')
   avatar.addEventListener('click', _ => {
