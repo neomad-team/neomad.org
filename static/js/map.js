78d5870c9315856da861bd710ffbe781b2365903
fuentesloic <fuentes.loic@gmail.com>
2017-09-05 19:53:43 -0400

    rewrite localizeUser controler

--- static/js/map.js
+++ static/js/map.js
@@ -9,7 +9,12 @@ const map = L.map('map', {
 })
 
 new L.Control.Zoom({position: 'topright'}).addTo(map)
-const localizeUser = L.control.locate({position: 'topright', setView: 'untilPan', locateOptions: {maxZoom: 14}}).addTo(map)
+
+const localizeUser = new L.control.locate({
+  position: 'topright',
+  setView: 'untilPan',
+  locateOptions: {maxZoom: 14}
+  }).addTo(map)
 
 // create icon model
 const iconOptions = {
