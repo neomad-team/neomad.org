49ecbca7d22c877813fd4d1e56aa0a39322f5253
fuentesloic <fuentes.loic@gmail.com>
2017-10-14 14:52:43 -0400

    init enhance card

--- static/components/PoiCard.js
+++ static/components/PoiCard.js
@@ -78,11 +78,11 @@ class PoiCard extends React.Component {
         onClick={this.clickCard}
         style={{order: this.calculateDistance(this.state.from, this.state.to)}}>
         <div className='card-distance'>{this.renderDistance(this.calculateDistance(this.state.from, this.state.to), this.state.unit)}</div>
-        <h2>{this.props.details.name}</h2>
+        <h2 title={this.props.details.name}>{this.props.details.name}</h2>
         <ul>
-          <Rank value={this.props.details.wifi} />
-          <Power value={this.props.details.power} />
-          <Comments value={this.props.details.comments} />
+          <li><Rank value={this.props.details.wifi} /></li>
+          <li><Power value={this.props.details.power} /></li>
+          <li><Comments value={this.props.details.comments} /></li>
         </ul>
       </div>
     )
