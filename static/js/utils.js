a82c7565e27b0d48d9eec4d13d058b395d54b47c
fuentesloic <fuentes.loic@gmail.com>
2017-10-27 06:29:36 -0400

    resolve css lost cause pull origin master

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,14 +1,14 @@
-function alert(type, message, delay) {
+function alert (type, message, delay) {
   console.info(type, message)
   let notification = document.querySelector('#notification')
-  if(!notification) {
+  if (!notification) {
     notification = document.createElement('div')
     notification.id = 'notification'
     document.body.append(notification)
   }
   notification.innerHTML = message
   notification.classList = [type]
-  if(delay !== 0) {
+  if (delay !== 0) {
     this.timer = setTimeout(_ => {
       notification.classList = []
     }, delay || 5000)
@@ -21,29 +21,11 @@ function coordinatesToAddress (coordinates) {
     mode: 'cors'
   })
   .then(r => r.json())
-<<<<<<< HEAD
   .then(d => {
     const data = d.address
     data['area'] = data.town || data.village || data.city
     return data
   })
-}
-
-function localize(url) {
-  navigator.geolocation.getCurrentPosition(position => {
-    fetch(url, {
-      method: 'post',
-      body: JSON.stringify([position.coords.latitude, position.coords.longitude]),
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-  })
-=======
-  .then(d => d.address)
-  .catch(console.error.bind(console))
->>>>>>> 9c4c40c29c6629a046c09e3785ba07ea18b97085
 }
 
 window.onload = _ => {
