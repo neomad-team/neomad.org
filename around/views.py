b4500b2e24a183c84e9fde77e267cf33af13e619
Loïc <fuentes.loic@gmail.com>
2018-02-15 22:05:11 -0500

    Users on map (#235)
    * add user on around map
    * display not current user

--- around/views.py
+++ around/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
 from core import app
+from user.models import User
 
 
 @app.route('/around/')
 def around():
-    return render_template('around/map.html')
+    users = User.objects(allow_community=True)
+    return render_template('around/map.html', users=users)
