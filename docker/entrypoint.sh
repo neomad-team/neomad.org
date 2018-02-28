3ba76ae1eedc0b2a30e4b65e317f3bf821b8c746
vinyll <vincent.agnano@scopyleft.fr>
2018-02-27 20:02:51 -0500

    removed yarn

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,11 @@
 
 # Add extra system dependencies
 apk --update add nodejs
-npm install yarn
 
 if [ -e requirements.txt ]; then
 	pip3 install -r requirements.txt
 fi
 
-# Build frontend assets
-yarn && yarn build
+npm install && npm run build
 
-gunicorn app:app -b :80 --name app
+gunicorn --workers 4 app:app -b :80 --name app
