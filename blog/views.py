ce49a61305336208231aeaa5a623a94956c70822
vinyll <vincent.agnano@scopyleft.fr>
2018-03-24 23:14:21 +0100

    added the editing article page

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from flask_login import current_user, login_required
 
-from core import app, db
+from core import app
 from core.helpers import url_for_user, url_for_article
 from user.models import User
 from .models import Article, clean_html
@@ -50,11 +50,11 @@ def article_create():
                 article.publication_date = None
             article.save()
             status = 201
-    return render_template('blog/article.html', article=article,
+    return render_template('blog/edit.html', article=article,
                            errors=errors, edit=True), status
 
 
-@app.route('/article/<string:id>/edit/', methods=['post'])
+@app.route('/article/<string:id>/edit/', methods=['get', 'post'])
 @login_required
 def article_edit(id):
     user = User.objects.get(id=current_user.id)
@@ -62,20 +62,21 @@ def article_edit(id):
         article = Article.objects.get(author=user, id=id)
     except Article.DoesNotExist:
         abort(404)
-    article.title = request.form.get('title')
-    article.content = clean_html(request.form.get('content'))
-    if request.form.get('published') != '':
-        article.publication_date = datetime.datetime.utcnow()
-    else:
-        article.publication_date = None
     errors = []
-    if article.title != '' and clean_html(article.content) != '':
-        article.save()
-        return redirect(url_for_article(article))
-    else:
-        errors.append('Please insert a title and a content')
-        return render_template('blog/article.html', article=article,
-                               errors=errors, edit=True), 400
+    if request.method == 'POST':
+        article.title = request.form.get('title')
+        article.content = clean_html(request.form.get('content'))
+        if request.form.get('published') != '':
+            article.publication_date = datetime.datetime.utcnow()
+        else:
+            article.publication_date = None
+        if article.title != '' and clean_html(article.content) != '':
+            article.save()
+            return redirect(url_for_article(article))
+        else:
+            errors.append('Please insert a title and a content')
+    return render_template('blog/edit.html', article=article,
+                           errors=errors, edit=True), 400
 
 
 @app.route('/article/<string:id>/delete/', methods=['get'])
